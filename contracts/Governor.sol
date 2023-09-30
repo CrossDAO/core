2d7281e83ab00c707a38a71dbd2554ca0eff188b
Yash Goyal <yashgo0018@gmail.com>
2023-09-30 15:54:21 +0200

    added code for the ccip sender and ccip receiver

--- contracts/Governor.sol
+++ contracts/Governor.sol
@@ -1,4 +1,98 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-contract Governor {}
+import {IRouterClient} from "@chainlink/contracts-ccip/src/v0.8/ccip/interfaces/IRouterClient.sol";
+import {OwnerIsCreator} from "@chainlink/contracts-ccip/src/v0.8/shared/access/OwnerIsCreator.sol";
+import {Client} from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.sol";
+import {LinkTokenInterface} from "@chainlink/contracts/src/v0.8/shared/interfaces/LinkTokenInterface.sol";
+import {CCIPReceiver} from "@chainlink/contracts-ccip/src/v0.8/ccip/applications/CCIPReceiver.sol";
+
+contract Governor is OwnerIsCreator, CCIPReceiver {
+    error NotEnoughBalance(uint256 currentBalance, uint256 calculatedFees);
+
+    event MessageSent(
+        bytes32 indexed messageId,
+        uint64 indexed destinationChainSelector,
+        address receiver,
+        string text,
+        address feeToken,
+        uint256 fees
+    );
+
+    event MessageReceived(
+        bytes32 indexed messageId,
+        uint64 indexed sourceChainSelector,
+        address sender,
+        string text
+    );
+
+    IRouterClient router;
+
+    LinkTokenInterface linkToken;
+
+    bytes32 private lastReceivedMessageId;
+    string private lastReceivedText;
+
+    constructor(address _router, address _link) CCIPReceiver(_router) {
+        router = IRouterClient(_router);
+        linkToken = LinkTokenInterface(_link);
+    }
+
+    function sendMessage(
+        uint64 destinationChainSelector,
+        address receiver,
+        string calldata text
+    ) external onlyOwner returns (bytes32 messageId) {
+        Client.EVM2AnyMessage memory evm2AnyMessage = Client.EVM2AnyMessage({
+            receiver: abi.encode(receiver),
+            data: abi.encode(text),
+            tokenAmounts: new Client.EVMTokenAmount[](0),
+            extraArgs: Client._argsToBytes(
+                Client.EVMExtraArgsV1({gasLimit: 200_000, strict: false})
+            ),
+            feeToken: address(linkToken)
+        });
+
+        uint256 fees = router.getFee(destinationChainSelector, evm2AnyMessage);
+
+        if (fees > linkToken.balanceOf(address(this)))
+            revert NotEnoughBalance(linkToken.balanceOf(address(this)), fees);
+
+        linkToken.approve(address(router), fees);
+
+        messageId = router.ccipSend(destinationChainSelector, evm2AnyMessage);
+
+        emit MessageSent(
+            messageId,
+            destinationChainSelector,
+            receiver,
+            text,
+            address(linkToken),
+            fees
+        );
+
+        return messageId;
+    }
+
+    function _ccipReceive(
+        Client.Any2EVMMessage memory any2EvmMessage
+    ) internal override {
+        lastReceivedMessageId = any2EvmMessage.messageId;
+        lastReceivedText = abi.decode(any2EvmMessage.data, (string));
+
+        emit MessageReceived(
+            any2EvmMessage.messageId,
+            any2EvmMessage.sourceChainSelector,
+            abi.decode(any2EvmMessage.sender, (address)),
+            abi.decode(any2EvmMessage.data, (string))
+        );
+    }
+
+    function getLastReceivedMessageDetails()
+        external
+        view
+        returns (bytes32 messageId, string memory text)
+    {
+        return (lastReceivedMessageId, lastReceivedText);
+    }
+}
