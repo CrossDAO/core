7625dc3410a0abb956e0e5cdc19e0048abd4d978
Cláudio Barreira <claudio.barreira89@gmail.com>
2023-10-07 22:57:55 +0100

    feat(interface): smart contract interactions

--- packages/nextjs/api/getProposals.ts
+++ packages/nextjs/api/getProposals.ts
@@ -1,7 +1,19 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
+const mockedProposals = [
+  {
+    id: 1,
+    title: "New proposal",
+    description: "This is some proposal that needs voting",
+    chainId: 1,
+  },
+];
+
 export const getProposals = async () => {
+  // For testing purposes
+  return mockedProposals;
+
   const response = await axios.get("/api/proposals").catch(err => {
     console.error(err);
     return { data: [] };
--- packages/nextjs/components/CreateProposal.tsx
+++ packages/nextjs/components/CreateProposal.tsx
@@ -1,8 +1,46 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
+import Spinner from "./Spinner";
+import toast from "react-hot-toast";
+import { Abi } from "viem";
+import { useNetwork } from "wagmi";
+import { prepareWriteContract, waitForTransaction, writeContract } from "wagmi/actions";
+import governanceAbi from "~~/abi/governanceAbi.json";
+import { governanceContract } from "~~/constants";
+import { queryClient } from "~~/services/react-query";
 
 const CreateProposal = () => {
+  const { chain } = useNetwork();
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
+
+  const [isCreating, setIsCreating] = useState(false);
+
+  const handleCreateProposalTransaction = useCallback(async () => {
+    if (!chain?.id) return;
+
+    setIsCreating(true);
+
+    try {
+      const { request } = await prepareWriteContract({
+        address: governanceContract[chain.id],
+        abi: governanceAbi as Abi,
+        functionName: "createProposal",
+        args: [[], [], [], `${title}:${description}`],
+      });
+
+      const { hash } = await writeContract(request);
+
+      await waitForTransaction({ hash });
+      await queryClient.invalidateQueries(["proposals"]);
+
+      toast.success("Proposal created");
+    } catch (err) {
+      console.error(err);
+      toast.error("Something went wrong", { id: "create-error" });
+    } finally {
+      setIsCreating(false);
+    }
+  }, [chain, description, title]);
 
   return (
     <div className="gap-4 flex flex-col pb-10">
@@ -24,6 +62,19 @@ const CreateProposal = () => {
           onChange={e => setDescription(e.target.value)}
         />
       </div>
+
+      {isCreating ? (
+        <div className="w-full flex justify-center">
+          <Spinner />
+        </div>
+      ) : (
+        <button
+          className="border rounded-full transition-colors border-base-200 hover:border-base-100  text-white px-4 py-2 text-center flex gap-2 justify-center"
+          onClick={() => handleCreateProposalTransaction()}
+        >
+          Create Proposal
+        </button>
+      )}
     </div>
   );
 };
